dbae5a8f4d5c9c2176bcc688ce51318a4cd89c6c
damien sauvalle <damiensauvalle@gmail.com>
2018-10-16 23:57:19 +0200

    chore : rename exempla --> main

--- src/main/java/webstationapi/Main.java
+++ src/main/java/webstationapi/Main.java
@@ -2,13 +2,12 @@ package webstationapi;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-public class Example {
+public class Main {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
 }
