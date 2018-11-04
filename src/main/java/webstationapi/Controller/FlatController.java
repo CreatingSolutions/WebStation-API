f28bc244c05b9523b4b17440d67d6f98e8a87929
damiensauv <damiensauvalle@gmail.com>
2018-11-05 00:33:19 +0100

    basic route Controller flats

---
+++ src/main/java/webstationapi/Controller/FlatController.java
@@ -0,0 +1,34 @@
+package webstationapi.Controller;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import webstationapi.Entity.Flat;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/flat")
+public class FlatController {
+
+    private String logementURL = "127.0.0.1:8083";
+
+    @GetMapping(path = "/")
+    public @ResponseBody
+    List<Flat> getFlats() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Flat>> response = restTemplate.exchange(
+                "http://localhost:8083/flats",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Flat>>() {
+                });
+        List<Flat> flats = response.getBody();
+        return flats;
+    }
+}
---
+++ src/main/java/webstationapi/Entity/Flat.java
@@ -0,0 +1,25 @@
+package webstationapi.Entity;
+
+
+
+public class Flat {
+
+    long id_flat;
+    String description;
+
+    public long getId_flat() {
+        return id_flat;
+    }
+
+    public void setId_flat(long id_flat) {
+        this.id_flat = id_flat;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
