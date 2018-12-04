ee82c7f4208b481dce89ce3e359888410dc953fd
damiensauv <damiensauvalle@gmail.com>
2018-12-04 23:55:43 +0100

    ref : SWaggerConfig

--- src/main/java/webstationapi/Config/SwaggerConfig.java
+++ src/main/java/webstationapi/Config/SwaggerConfig.java
@@ -2,11 +2,17 @@ package webstationapi.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.security.Principal;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -14,10 +20,15 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2).ignoredParameterTypes(Principal.class, UsernamePasswordAuthenticationToken.class)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build().securitySchemes(Collections.singletonList(apiKey()));
     }
+
+    private ApiKey apiKey(){
+        return new ApiKey("tokenOAuth", "Authorization", "header");
+    }
+
 }
---
+++ src/main/java/webstationapi/DTO/TokenDTO.java
@@ -0,0 +1,11 @@
+package webstationapi.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenDTO {
+
+    String token;
+}
