6eaf7bfb00b83b2034b2ff5d35e524aad39c27e3
damiensauv <damiensauvalle@gmail.com>
2018-12-04 22:52:37 +0100

    feat : make Exception Handler

---
+++ src/main/java/webstationapi/Exception/WebStationException.java
@@ -0,0 +1,13 @@
+package webstationapi.Exception;
+
+public class WebStationException extends RuntimeException {
+
+    WebStationException() {
+        super();
+    }
+
+    public WebStationException(String message) {
+        super(message);
+    }
+
+}
