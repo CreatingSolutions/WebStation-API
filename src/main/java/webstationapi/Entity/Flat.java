e697a2eeabe598e8b74defefd341491089a3d9e4
damiensauv <damiensauvalle@gmail.com>
2018-12-14 23:10:00 +0100

    feat : up flat

--- src/main/java/webstationapi/Entity/Flat.java
+++ src/main/java/webstationapi/Entity/Flat.java
@@ -4,6 +4,11 @@ public class Flat {
 
     long idFlat;
     String description;
+    String nbPersonnes;
+    boolean sdbwc;
+    boolean pets;
+    boolean wifi;
+    String orientation;
 
     public long getIdFlat() {
         return idFlat;
@@ -19,5 +24,49 @@ public class Flat {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setIdFlat(long idFlat) {
+        this.idFlat = idFlat;
+    }
+
+    public String getNbPersonnes() {
+        return nbPersonnes;
+    }
+
+    public void setNbPersonnes(String nbPersonnes) {
+        this.nbPersonnes = nbPersonnes;
+    }
+
+    public boolean isSdbwc() {
+        return sdbwc;
+    }
+
+    public void setSdbwc(boolean sdbwc) {
+        this.sdbwc = sdbwc;
+    }
+
+    public boolean isPets() {
+        return pets;
+    }
+
+    public void setPets(boolean pets) {
+        this.pets = pets;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
     }
 }
