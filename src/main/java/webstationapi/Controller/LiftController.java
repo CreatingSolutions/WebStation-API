8fb13551fe7504834b39923539156827f5315815
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-03-11 17:47:03 +0100

    wip

--- src/main/java/webstationapi/Controller/LiftController.java
+++ src/main/java/webstationapi/Controller/LiftController.java
@@ -1,5 +1,6 @@
 package webstationapi.Controller;
 
+import com.itextpdf.text.DocumentException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
@@ -8,10 +9,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import webstationapi.DTO.LiftDTO;
 import webstationapi.Entity.Lift;
+import webstationapi.Entity.LiftBooking;
 import webstationapi.Enum.AgeEnum;
 import webstationapi.Enum.TypeEnum;
 
-import java.util.Arrays;
+import java.io.FileNotFoundException;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,6 +23,8 @@ public class LiftController {
 
     @Value("${api.lift.url}")
     private String baseUrl;
+
+    // Lift
 
     @GetMapping
     public List<Lift> getLiftByTypeAndAge(@RequestParam(value = "type") TypeEnum type, @RequestParam(value = "age") AgeEnum age) {
@@ -66,5 +70,37 @@ public class LiftController {
         return response.getBody();
     }
 
+    // Booking
+
+    @GetMapping("/liftbooking/{id}")
+    public List<LiftBooking> getActiveBookByUser(@PathVariable Long id) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<LiftBooking>> exchange = restTemplate.exchange(baseUrl + "/liftbooking/" + id,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<LiftBooking>>() {
+                });
+        return exchange.getBody();
+    }
+
+    @PostMapping("/liftbooking/{idUser}")
+    public void makeBook(@RequestBody List<LiftDTO> liftDTOS, @PathVariable Long idUser) {
+        RestTemplate template = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<List<LiftDTO>> requestEntity = new HttpEntity<>(liftDTOS, headers);
+
+        template.exchange(baseUrl + "/liftbooking/" + idUser, HttpMethod.POST, requestEntity, Object.class);
+    }
+
+    @GetMapping("/liftbooking/pdf/{iduser}")
+    public void generatePdf(@PathVariable Long iduser) throws FileNotFoundException, DocumentException {
+        // also validate book
+        // Need TO Generate IN API
+        //this.liftBookingService.makePdf(iduser);
+    }
 
 }
