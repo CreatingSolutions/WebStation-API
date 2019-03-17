6511f5b98a185b93faba6058b0eaac91fd157739
damiensauv <damiensauvalle@gmail.com>
2019-03-17 16:48:42 +0100

    update

--- src/main/java/webstationapi/Controller/LiftController.java
+++ src/main/java/webstationapi/Controller/LiftController.java
@@ -7,6 +7,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import webstationapi.DTO.ForfaitDTO;
 import webstationapi.DTO.LiftDTO;
 import webstationapi.Entity.Lift;
 import webstationapi.Entity.LiftBooking;
@@ -14,37 +15,76 @@ import webstationapi.Enum.AgeEnum;
 import webstationapi.Enum.TypeEnum;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/lift")
+@RequestMapping(path = "/lifts")
 public class LiftController {
 
     @Value("${api.lift.url}")
     private String baseUrl;
 
-    // Lift
 
-    @GetMapping
-    public List<Lift> getLiftByTypeAndAge(@RequestParam(value = "type") TypeEnum type, @RequestParam(value = "age") AgeEnum age) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl + "/lift")
-                .queryParam("type", type)
-                .queryParam("age", age);
+    private TypeEnum getType(String type) {
+        if ("skialpin".equals(type))
+            return TypeEnum.ALPIN;
+        else if ("skinordique".equals(type))
+            return TypeEnum.NORDIQUE;
+        else
+            return TypeEnum.TELESIEGE;
+    }
 
+    private AgeEnum getAge(String age) {
+        if ("jeunes".equals(age))
+            return AgeEnum.JEUNE;
+        else if ("adultes".equals(age))
+            return AgeEnum.ADULTE;
+        else if ("senior".equals(age))
+            return AgeEnum.SENIOR;
+        else
+            return AgeEnum.AGE_OR;
+    }
+
+    private UriComponentsBuilder buildUrl(TypeEnum typeEnum, AgeEnum ageEnum) {
+        return UriComponentsBuilder.fromHttpUrl(baseUrl + "/lift")
+                .queryParam("type", typeEnum)
+                .queryParam("age", ageEnum);
+    }
+
+    private LiftDTO makeLiftRequest(UriComponentsBuilder builder) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<Lift>> exchange = restTemplate.exchange(
+        ResponseEntity<LiftDTO> exchange = restTemplate.exchange(
                 builder.toUriString(),
                 HttpMethod.GET,
                 entity,
-                new ParameterizedTypeReference<List<Lift>>() {
+                new ParameterizedTypeReference<LiftDTO>() {
                 });
         return exchange.getBody();
     }
+
+    @GetMapping("/{type}/{age}")
+    public LiftDTO getAllForfait(@PathVariable("type") String type, @PathVariable("age") String age) {
+
+        TypeEnum typeEnum = this.getType(type);
+        AgeEnum ageEnum = this.getAge(age);
+
+        UriComponentsBuilder builder = this.buildUrl(typeEnum, ageEnum);
+        return this.makeLiftRequest(builder);
+    }
+
+    @GetMapping("/telesiege")
+    public LiftDTO getTelesiege() {
+
+        UriComponentsBuilder builder = this.buildUrl(TypeEnum.TELESIEGE, AgeEnum.JEUNE);
+        return this.makeLiftRequest(builder);
+    }
+
 
     @GetMapping("{id}")
     public Lift getLift(@PathVariable Long id) {
