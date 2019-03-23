c380d75988a13889d856a42bfb6b308826a03247
damiensauv <damiensauvalle@gmail.com>
2019-03-23 13:52:34 +0100

    wip

---
+++ src/main/java/webstationapi/DTO/StuffBookDTO.java
@@ -0,0 +1,14 @@
+package webstationapi.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StuffBookDTO {
+
+    private Long stuffId;
+
+    private int taked;
+
+}
